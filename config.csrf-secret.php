6eee5271ddd78c0b01a0b521c8c290ff276b99c5
Sanjay Das <dassanjay0130@gmail.com>
2025-10-04 16:12:54 +0530

    File deleted

--- config.csrf-secret.php
+++
@@ -1,1 +0,0 @@
-<?php $secret = "54dbeb07c8bec190c4a84621075f3fa02113e5ae";
--- user_privileges/user_privileges_6.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-//This is the access privilege file
-$is_admin=true;
-
-$user_info=array('user_name'=>'Antariksh','is_admin'=>'on','user_password'=>'$1$An000000$TWCNDHB.VeyO6JdnsXqlL0','confirm_password'=>'$1$An000000$TWCNDHB.VeyO6JdnsXqlL0','first_name'=>'Antariksh','last_name'=>'Aggarwal','roleid'=>'H2','email1'=>'ant.role@gmail.com','status'=>'Active','activity_view'=>'This Month','lead_view'=>'Today','hour_format'=>'24','end_hour'=>'','start_hour'=>'09:00','title'=>'','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'dd-mm-yyyy','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'iJ8NoSpUqu1TgXGG','time_zone'=>'Asia/Kolkata','currency_id'=>'1','currency_grouping_pattern'=>'12,34,56,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','imagename'=>'','internal_mailer'=>'0','theme'=>'softed','language'=>'en_us','reminder_interval'=>'1 Minute','phone_crm_extension'=>'','no_of_currency_decimals'=>'2','truncate_trailing_zeros'=>'0','dayoftheweek'=>'Monday','callduration'=>'5','othereventduration'=>'5','calendarsharedtype'=>'public','default_record_view'=>'Summary','leftpanelhide'=>'0','rowheight'=>'medium','defaulteventstatus'=>'Held','defaultactivitytype'=>'Call','hidecompletedevents'=>'1','is_owner'=>'1','currency_name'=>'India, Rupees','currency_code'=>'INR','currency_symbol'=>'₹','conv_rate'=>'1.00000','record_id'=>'','record_module'=>'','id'=>'6');
-
-?>
